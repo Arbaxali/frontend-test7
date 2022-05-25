94c689d1c53cb99ff4f7f68b90c92844dffd9c70
Arbaz ali <30619186+Arbaxali@users.noreply.github.com>
2022-05-26 02:43:46 +0530

    frontend-popup Assignemnt

---
+++ index.js
@@ -0,0 +1,10 @@
+var modal = document.getElementById("this.id");
+
+var btn = document.getElementById("this.id");
+
+var span = document.getElementsByClassName("close")[0];
+
+btn.onclick = function() {
+    modal.style.display = "block";
+}
+
